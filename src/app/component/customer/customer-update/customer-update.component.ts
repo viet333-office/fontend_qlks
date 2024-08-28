44ebbbfcb0810d1a355dc5b3e11a85645375e047
viet333-office <tran.viet.hn2003@gmail.com>
2024-08-28 18:35:41 +0700

    viet

--- src/app/component/customer/customer-update/customer-update.component.ts
+++ src/app/component/customer/customer-update/customer-update.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, Output  } from '@angular/core';
 import { Customer } from '../../../Interface/customer';
 import { CustomerServiceService } from '../../../Service/logic/customer-service.service'
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-update',
@@ -21,7 +21,49 @@ export class CustomerUpdateComponent {
   @Output() loadingChange = new EventEmitter<boolean>();
 
   constructor(private fb: FormBuilder, private customerService: CustomerServiceService) { }
+  customerForm = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(/^[^!@#$%^&*(),.?":{}|<>]*$/), Validators.pattern(/^[^\d]+$/), Validators.pattern(/^\s*$/), Validators.minLength(3), Validators.maxLength(20)]],
+    phone: ['', [Validators.required, Validators.pattern(/^(03|09|02)\d{8}$/)]],
+    address: ['', [Validators.required, Validators.pattern(/^[^!@#$%^&*(),.?":{}|<>]*$/), Validators.pattern(/^\s*$/), Validators.minLength(5), Validators.maxLength(50)]],
+    cccd: ['', [Validators.required, Validators.pattern(/^\d{12}$/)]]
+  });
+
+  get nameError() {
+    const nameControl = this.customerForm.get('name');
+    if (nameControl?.errors && nameControl.dirty) {
+      const value = (nameControl.value || '');
+      if (/\d/.test(value)) {
+        return 'Không được chứa số';
+      } else if (/[^a-zA-Z0-9\s]/.test(value)) {
+        return 'Không được chứa ký tự đặc biệt';
+      } else if (/^\s*$/.test(value)) {
+        return 'phải nhập kí tự không được để khoảng trắng';
+      }
+    }
+    return null;
+  }
+
+  get addressError() {
+    const addressControl = this.customerForm.get('address');
+    if (addressControl?.errors && addressControl.dirty) {
+      const value = (addressControl.value || '');
+      if (/^\s*$/.test(value)) {
+        return 'phải nhập kí tự không được để khoảng trắng ';
+      } else if (/[^a-zA-Z0-9\s]/.test(value)) {
+        return 'Không được chứa ký tự đặc biệt';
+      }
+    }
+    return null;
+  }
+  hideDialog() {
+    this.visible = false;
+    this.customerForm.reset();
+    this.visibleChange.emit(this.visible);
+  }
+
   updateCustomer(customer: Customer) {
+    console.log(customer,"log");
+    
     this.loadingChange.emit(true);
     this.customerService.putCustomer(customer).subscribe(
       () => {
